f577a8a3c59b6e44508cfaa49e2893cd4fc8510b
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-10-15 18:31:49 +0530

    feat:media query added

--- app.js
+++ app.js
@@ -7,24 +7,29 @@ arrows.forEach((arrow, i) => {
   let clickCounter = 0;
 
   arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
     clickCounter++;
-    if (itemNumber - (6 + clickCounter) >= 0) {
+    if (itemNumber - (6 + clickCounter) + (6 - ratio) >= 0) {
       movieLists[i].style.transform = `translateX(${
         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
-    }else{
-        movieLists[i].style.transform = "translateX(0)";
-        clickCounter=0;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
     }
   });
 });
 
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(".container, .movie-list-title, .navbar-container, .sidebar, .left-menu-icon, .toggle");
+// toggle
 
-ball.addEventListener("click", ()=> {
-    items.forEach(item=> {
-        item.classList.toggle("active");
-    })
-    ball.classList.toggle("active")
-})
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container, .movie-list-title, .navbar-container, .sidebar, .left-menu-icon, .toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
